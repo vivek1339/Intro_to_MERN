e0ffc013c6adeb0ff10f8098ea104f2bb1cbd480
SaSha1724 <SasiSharma1724@users.noreply.github.com>
2023-03-31 01:58:38 -0400

    Bug_fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { List, Input, Button, Spin, message } from 'antd';
+import { List, Input, Button, Spin, message, Modal } from 'antd';
 import axios from 'axios';
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [updateModalVisible, setUpdateModalVisible] = useState(false);
+  const [updateItem, setUpdateItem] = useState({ id: '', name: '' });
+  const [updateValue, setUpdateValue] = useState('');
 
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:3001/api/get_all')
+      .get('http://localhost:3002/api/get_all')
       .then(res => {
         setItems(res.data);
         setLoading(false);
@@ -23,12 +26,12 @@ const App = () => {
   }, []);
 
   const addItem = () => {
-    // alert("hi")
     setLoading(true);
     axios
-      .post('http://localhost:3001/api/add', { name: inputValue }, {headers:{'Access-Control-Allow-Credentials':'true'}})
+      .post('http://localhost:3002/api/add', { name: inputValue })
       .then(res => {
-        setItems([res.data, ...items]);
+        const newItem = { _id: res.data.id, name: inputValue };
+        setItems([newItem, ...items]);
         setLoading(false);
         setInputValue('');
         message.success('Item added');
@@ -39,11 +42,12 @@ const App = () => {
         message.error('Failed to add item');
       });
   };
+  
 
   const deleteItem = id => {
     setLoading(true);
     axios
-      .delete(`http://localhost:3001/deletedata/${id}`)
+      .post(`http://localhost:3002/api/delete`, { id })
       .then(() => {
         setItems(items.filter(item => item._id !== id));
         setLoading(false);
@@ -56,6 +60,31 @@ const App = () => {
       });
   };
 
+  const updateCurrentItem = () => {
+    setLoading(true);
+    axios
+      .post(`http://localhost:3002/api/update`, { id: updateItem.id, name: updateValue })
+      .then(() => {
+        const updatedItems = items.map(item => {
+          if (item._id === updateItem.id) {
+            return { ...item, name: updateValue };
+          }
+          return item;
+        });
+        setItems(updatedItems);
+        setLoading(false);
+        setUpdateModalVisible(false);
+        setUpdateItem({ id: '', name: '' });
+        setUpdateValue('');
+        message.success('Item updated');
+      })
+      .catch(err => {
+        console.error(err);
+        setLoading(false);
+        message.error('Failed to update item');
+      });
+  };
+
   return (
     <div className="container">
       <h1 className="text-center my-5">To-Do List</h1>
@@ -65,6 +94,13 @@ const App = () => {
           renderItem={item => (
             <List.Item
               actions={[
+                <Button type="primary" onClick={() => {
+                  setUpdateItem({ id: item._id, name: item.name });
+                  setUpdateValue(item.name);
+                  setUpdateModalVisible(true);
+                }}>
+                  Edit
+                </Button>,
                 <Button type="danger" onClick={() => deleteItem(item._id)}>
                   Delete
                 </Button>
@@ -80,13 +116,28 @@ const App = () => {
             onChange={e => setInputValue(e.target.value)}
             placeholder="Add a new item"
           />
-          <Button type="primary" onClick={addItem}>
-            Add
-          </Button>
-        </div>
-      </Spin>
+          <Button type="primary" onClick={addItem}>        Add
+      </Button>
     </div>
-  );
+  </Spin>
+  <Modal
+    title="Update Item"
+    visible={updateModalVisible}
+    onOk={updateCurrentItem}
+    onCancel={() => {
+      setUpdateModalVisible(false);
+      setUpdateItem({ id: '', name: '' });
+      setUpdateValue('');
+    }}
+  >
+    <Input
+      value={updateValue}
+      onChange={e => setUpdateValue(e.target.value)}
+      placeholder="Update item"
+    />
+  </Modal>
+</div>
+);
 };
 
 export default App;
--- toDoBackend/app.js
+++ toDoBackend/app.js
@@ -1,40 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var cors = require("cors");
+var mongoose = require("mongoose");
+var createError = require("http-errors");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-const cors = require('cors');
+var allowedOrigins = ["http://localhost:3000"];
 app.use(cors({
-    origin: '*'
-}));
-const corsOptions = {
-  origin: true,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
-}
-app.options('*', cors(corsOptions));
-const mongoose = require('mongoose');
+}));
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 mongoose
-  .connect('mongodb+srv://express_user:express_user@cluster0.fnfbvfq.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
+  .connect("mongodb+srv://express_user:express_user@cluster0.fnfbvfq.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 // catch 404 and forward to error handler
@@ -46,11 +46,15 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.listen(3001, function() {
+  console.log("API server listening on port 3001");
 });
 
 module.exports = app;
