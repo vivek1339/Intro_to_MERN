f350ed616ce01f073d95d10ab704c8b1643f5b0d
vivek1339 <kvivek1339@gmail.com>
2023-03-31 22:58:31 +0530

    fixed cors errors

--- toDoBackend/app.js
+++ toDoBackend/app.js
@@ -15,15 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 var allowedOrigins = ["http://localhost:3000"];
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
+  origin: '*'
 }));
 app.options("*", (req, res) => {
   res.sendStatus(200);
